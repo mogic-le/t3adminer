acacc20104ad520e0688d318633d9e7cf3004bd2
secured3 <elgato@secured.de>
2024-10-30 10:13:22 +0000

    move Hooks to Eventlistener

--- Classes/EventListener/AfterUserLoggedOut.php
+++ Classes/EventListener/AfterUserLoggedOut.php
@@ -10,6 +10,29 @@ final class AfterUserLoggedOut
 {
     public function __invoke(AfterUserLoggedOutEvent $event): void
     {
-        \Jigal\T3adminer\Hooks\T3AdminerHooks::class . '->logoffHook';
+        $AbstractUserAuthentication = $event->getUser();
+
+        if (isset($_SESSION)) {
+            session_write_close();
+        }
+        if ($sessionId = $_COOKIE['tx_t3adminer'] ?? null) {
+            session_id($sessionId);
+            session_start();
+            unset(
+                $_SESSION['pwds'],
+                $_SESSION['ADM_driver'],
+                $_SESSION['ADM_user'],
+                $_SESSION['ADM_password'],
+                $_SESSION['ADM_server'],
+                $_SESSION['ADM_db'],
+                $_SESSION['ADM_tca'],
+                $_SESSION['ADM_extConf'],
+                $_SESSION['ADM_hideOtherDBs'],
+                $_SESSION['ADM_SignonURL'],
+                $_SESSION['ADM_uploadDir']
+            );
+            session_write_close();
+            $AbstractUserAuthentication->removeCookie('tx_t3adminer');
+        }
     }
 }
