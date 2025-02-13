c61955206c2b297ad951b464e0f9320723f373b8
Christian Weiske <weiske@mogic.com>
2025-02-13 17:42:16 +0100

    [BUGFIX] Fix logout handling on TYPO3v12
    In TYPO3v12, "logoff_post_processing" hook has been deprecated.
    We migrate to the "BeforeUserLogoutEvent" which allows us to clean
    up sensitive data from the session as before.
    
    For admin users, the Install tool session handler is invoked during
    the logout process. The session save path gets modified during that.
    To be able to clean the session from sensitive data, we have to
    store the original session save path in the backend user session.
    
    Changelog:
    https://docs.typo3.org/c/typo3/cms-core/main/en-us/Changelog/12.3/Deprecation-100307-VariousHooksRelatedToAuthenticationUsers.html
    https://docs.typo3.org/c/typo3/cms-core/main/en-us/Changelog/12.4/Deprecation-100173-VariousMethodsAndPropertiesInUserAuthenticationClassesNowInternal.html
    
    Resolves: https://gitlab.com/jigal/t3adminer/-/issues/32

--- Classes/EventListener/BeforeUserLogout.php
+++ Classes/EventListener/BeforeUserLogout.php
@@ -4,23 +4,40 @@ declare(strict_types=1);
 
 namespace Jigal\T3adminer\EventListener;
 
-use TYPO3\CMS\Core\Authentication\Event\AfterUserLoggedOutEvent;
+use TYPO3\CMS\Core\Authentication\Event\BeforeUserLogoutEvent;
 
-final class AfterUserLoggedOut
+/**
+ * Event handler for TYPO3v12+.
+ * v11 handling is in Hooks\T3AdminerHooks
+ */
+final class BeforeUserLogout
 {
-    public function __invoke(AfterUserLoggedOutEvent $event): void
+    /**
+     * Hook to remove t3adminer session on logoff
+     */
+    public function __invoke(BeforeUserLogoutEvent $event): void
     {
         if (isset($_SESSION)) {
             session_write_close();
         }
         if ($sessionId = $_COOKIE['tx_t3adminer'] ?? null) {
+            //restore session save path, because CMS installtool sets its own
+            // save path in SessionService() during admin user logout
+            $savePath = $GLOBALS['BE_USER']->getSessionData('t3adminer_session_savepath');
+            if ($savePath != '') {
+                session_save_path($savePath);
+            }
+
+            session_set_save_handler(new \SessionHandler(), true);
             session_id($sessionId);
             session_start();
+
             unset(
                 $_SESSION['pwds'],
                 $_SESSION['ADM_driver'],
                 $_SESSION['ADM_user'],
                 $_SESSION['ADM_password'],
+                $_SESSION['ADM_port'],
                 $_SESSION['ADM_server'],
                 $_SESSION['ADM_db'],
                 $_SESSION['ADM_tca'],
@@ -30,6 +47,8 @@ final class AfterUserLoggedOut
                 $_SESSION['ADM_uploadDir']
             );
             session_write_close();
+
+            //remove the cookie
             setcookie('tx_t3adminer', '', 1, '/');
         }
     }
