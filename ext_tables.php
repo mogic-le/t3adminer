b9c83de46a4545e24f4a8fcd14fd9d3f7a2e193a
Timothy Elias <elias+git@mogic.com>
2026-07-14 15:21:18 +0000

    [TASK] Remove dead TYPO3 v11 module registration from ext_tables.php
    Module registration is handled by Configuration/Backend/Modules.php
    since v12. The v11 code path is dead code and addModule() no longer
    exists in v13.

--- ext_tables.php
+++ ext_tables.php
@@ -1,28 +1,2 @@
 <?php
 defined('TYPO3') or die();
-
-use TYPO3\CMS\Core\Information\Typo3Version;
-use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
-
-call_user_func(static function() {
-
-    $versionInformation = GeneralUtility::makeInstance(Typo3Version::class);
-
-    if ($versionInformation->getMajorVersion() == 11) {
-        // registration for v11
-        ExtensionManagementUtility::addModule(
-            'tools',
-            'txt3adminerM1',
-            '',
-            null,
-            [
-                'routeTarget' => Jigal\T3adminer\Controller\AdminerController::class . '::main',
-                'access' => 'systemMaintainer',
-                'name' => 'tools_txt3adminerM1',
-                'labels' => 'LLL:EXT:t3adminer/Resources/Private/Language/locallang_mod.xlf',
-                'iconIdentifier' => 'adminer-module',
-            ]
-        );
-    }
-});
